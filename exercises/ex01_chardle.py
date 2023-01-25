3b637b3320745218ba565506e6a536396a459091
camryn ekonomon <cekonomon@unc.edu>
2023-01-24 23:05:08 -0500

    Progress on Exercise 1

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,40 @@
+"""EX01 - Chardle - A cute step toward wordle."""
+
+__author__ = "730529946"
+
+five_letter_word: str = input("Enter a 5-character word: ")
+if len(five_letter_word) !=5:
+    print("Error: word must contain 5 characters")
+else:
+    single_character = input("Enter a single character: ")
+    if len(single_character) !=1:
+        print("Error: Character must be a single character.")
+
+
+
+print("Searching for " + single_character + " in " + five_letter_word)
+
+
+if (single_character == five_letter_word[0]):
+    print(single_character + " found at index 0")
+if (single_character == five_letter_word[1]):
+    print(single_character + " found at index 1")
+if (single_character == five_letter_word[2]):
+    print(single_character + " found at index 2")
+if (single_character == five_letter_word[3]):
+    print(single_character + " found at index 3")
+if (single_character == five_letter_word[4]):
+    print(single_character + " found at index 4")
+
+if (single_character == five_letter_word[0]):
+    print ("1 instance of " + single_character + " found in " + five_letter_word)
+if (single_character == five_letter_word[1]):
+    print ("1 instance of " + single_character + " found in " + five_letter_word)
+if (single_character == five_letter_word[2]): 
+    print ("1 instance of " + single_character + " found in " + five_letter_word)
+if (single_character == five_letter_word[3]):
+    print ("1 instance of " + single_character + " found in " + five_letter_word)
+if (single_character == five_letter_word[4]):
+    print ("1 instance of " + single_character + " found in " + five_letter_word)
+
+
