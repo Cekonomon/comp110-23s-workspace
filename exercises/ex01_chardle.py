dc3e5ec494bb066fcc9d302d0c2534e850e4630f
camryn ekonomon <cekonomon@unc.edu>
2023-01-24 23:29:17 -0500

    progress on exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,23 +1,16 @@
 """EX01 - Chardle - A cute step toward wordle."""
 
-__author__ = "730529946"
-
+__author__ = "730529946"  
+ 
 five_letter_word: str = input("Enter a 5-character word: ")
-if len(five_letter_word) !=5:
-    print("Error: word must contain 5 characters")
-else:
-    single_character = input("Enter a single character: ")
-    if len(single_character) !=1:
-        print("Error: Character must be a single character.")
-
-
-
+single_character: str = input("Enter a single character:") 
+ 
 print("Searching for " + single_character + " in " + five_letter_word)
 
 
 if (single_character == five_letter_word[0]):
     print(single_character + " found at index 0")
-if (single_character == five_letter_word[1]):
+if (single_character == five_letter_word[1]): 
     print(single_character + " found at index 1")
 if (single_character == five_letter_word[2]):
     print(single_character + " found at index 2")
