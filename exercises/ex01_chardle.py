a86b4dc3bf80919e18d1e4186d9861efd4426bc8
camryn ekonomon <cekonomon@unc.edu>
2023-01-28 18:06:13 -0500

    Progress on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -16,16 +16,16 @@ else:
         print("Searching for ", single_character, " in ", five_letter_word)
  
 
-if (single_character == five_letter_word[0]):
-    print(single_character, " found at index 0")
-if (single_character == five_letter_word[1]): 
-    print(single_character, " found at index 1")
-if (single_character == five_letter_word[2]):
-    print(single_character, " found at index 2")
-if (single_character == five_letter_word[3]):
-    print(single_character, " found at index 3")
-if (single_character == five_letter_word[4]):
-    print(single_character, " found at index 4")
+if single_character == five_letter_word[0]:
+    print(single_character + " found at index 0")
+if single_character == five_letter_word[1]: 
+    print(single_character + " found at index 1")
+if single_character == five_letter_word[2]:
+    print(single_character + " found at index 2")
+if single_character == five_letter_word[3]:
+    print(single_character + " found at index 3")
+if single_character == five_letter_word[4]:
+    print(single_character + " found at index 4")
 
 if five_letter_word[0] == single_character:
     instance_one = int(1)
