0f63ac66c84f57201fad0f6a452f257b43b7e273
camryn ekonomon <cekonomon@unc.edu>
2023-01-28 17:43:14 -0500

    Progress on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -17,15 +17,15 @@ else:
  
 
 if (single_character == five_letter_word[0]):
-    print(single_character + " found at index 0")
+    print(single_character , " found at index 0")
 if (single_character == five_letter_word[1]): 
-    print(single_character + " found at index 1")
+    print(single_character , " found at index 1")
 if (single_character == five_letter_word[2]):
-    print(single_character + " found at index 2")
+    print(single_character , " found at index 2")
 if (single_character == five_letter_word[3]):
-    print(single_character + " found at index 3")
+    print(single_character , " found at index 3")
 if (single_character == five_letter_word[4]):
-    print(single_character + " found at index 4")
+    print(single_character , " found at index 4")
 
 if five_letter_word[0] == single_character:
     instance_one = int(1)
