2869e41f943b621f4ffd950d2452e1edf37ddef6
camryn ekonomon <cekonomon@unc.edu>
2023-01-28 17:33:40 -0500

    Progress on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -13,7 +13,7 @@ else:
         print("Error: Character must contain 1 letter")
         exit()
     else:
-        print("Searching for " + single_character + " in " + five_letter_word)
+        print("Searching for " , single_character , " in " + five_letter_word)
  
 
 if (single_character == five_letter_word[0]):
@@ -28,22 +28,37 @@ if (single_character == five_letter_word[4]):
     print(single_character + " found at index 4")
 
 if five_letter_word[0] == single_character:
-    instance_one = int (1)
+    instance_one = int(1)
 else:
-    instance_one = int (0)
+    instance_one = int(0)
 if five_letter_word[1] == single_character:
-    instance_one = int (1)
+    instance_two = int(1)
 else:
-    instance_one = int (0)
+    instance_two = int(0)
 if five_letter_word[2] == single_character:
-    instance_one = int (1)
+    instance_three = int(1)
 else:
-    instance_one = int (0)
+    instance_three = (0)
 if five_letter_word[3] == single_character:
-    instance_one = int (1)
+    instance_four = int(1)
 else:
-    instance_one = int (0)  
+    instance_four = int(0)
 if five_letter_word[4] == single_character:
-    instance_one = int (1)
+    instance_five = int(1)
 else:
-    instance_one = int (0)  
+    instance_five = (0)
+
+total_instances = int(instance_one + instance_two + instance_three + instance_four + instance_five)
+
+if total_instances == 0:
+    print("No instances of", single_character, "found in", five_letter_word)
+if total_instances == 1:
+    print(total_instances , "instances of", single_character, "found in", five_letter_word)
+if total_instances == 2:
+    print(total_instances , "instances of", single_character, "found in", five_letter_word)
+if total_instances == 3:
+    print(total_instances , "instances of", single_character, "found in", five_letter_word)
+if total_instances == 4:
+    print(total_instances , "instances of", single_character, "found in", five_letter_word)
+if total_instances == 5:
+    print(total_instances , "instances of", single_character, "found in", five_letter_word)
