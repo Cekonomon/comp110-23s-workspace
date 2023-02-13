b950b4a4a740606900f4e25f8608091b5535a997
camryn ekonomon <cekonomon@unc.edu>
2023-02-13 17:41:31 -0500

    Progress on Exercise 1

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -3,31 +3,38 @@
 __author__ = "730529946"
 
 SECRET: str = "python" 
+len_secret: int = len(SECRET)
 guess: str = str(input("What is your 6 letter guess? "))
 playing: bool = True 
 index_variable: int = 0 
+alt_idx: int = 0
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
-emoji_string: str = "" 
+result: str = "" 
 
-#checking boxes index
-while index_variable < len(SECRET):
-    if guess == SECRET:
-        print(GREEN_BOX)
-        index_variable = index_variable + 1
-    else:
-        print(WHITE_BOX)
-        index_variable = index_variable + 1
-
-#checking correct word
+# correct color boxes 
 while playing:
-    if guess == SECRET:
+      while len(guess) != len_secret:
+          guess: str = str(input("That was not 6 letters! Try again soon: "))
+
+      while index_variable < len_secret:
+          if (guess == SECRET):
+              result = result + GREEN_BOX
+              playing = False
+          else: 
+            result = result + WHITE_BOX
+            playing = False
+          index_variable = index_variable + 1 
+print(result)
+
+playing: bool = True
+# telling player if it is right or wrong guess
+while playing:
+    if (guess == SECRET):
         print("Woo! You got it! ")
         playing = False
     else:
-        if len(guess) != len(SECRET):
-            guess = input("that was not 6 letters! Try again soon: ")
-        if len(guess) == len(SECRET):
+        if len(guess) == len_secret:
             print("Not quite. Play again soon! ")
-            guess = input("What is your 6 letter guess? ")
+            playing = False
