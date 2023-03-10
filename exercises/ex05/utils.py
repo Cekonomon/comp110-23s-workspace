ca0c68aa6c5805b97c38d02a5f3229005b124c1e
camryn ekonomon <cekonomon@unc.edu>
2023-03-10 10:23:11 -0500

    Progress on Exercise

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,3 +1,4 @@
+"""List practice."""
 __author__ = "730529946"
 
 def only_evens(list_evens: list[int]) -> int:
@@ -10,19 +11,23 @@ def only_evens(list_evens: list[int]) -> int:
 
 def concat(list_one: list[int], list_two: list[int]) -> list[int]:
     """Two list together."""
-    for number in list_two:
-        list_one.append(number)
-    print(str(list_one))
+    lists: list[int] = []
+    for number in list_one:
+        lists.append(number)
+    for numbers in list_two:
+        lists.append(numbers)
+    return lists
 
 def sub(a_list: list[int], start_idx: int, end_idx: int) -> list[int]:
     """Generate list which is sub of given list."""
-    subset: list[int] = []
+    subset: list[int] = ()
+    if start_idx < 0:
+        start_idx = 0
+    if len(subset) < end_idx:
+        end_idx = len(subset)
+        
+    if len(subset) == 0 or start_idx >= len(subset) or end_idx <= 0:
+        return []
     for idx in range(start_idx, end_idx):
-        subset.append(a_list[idx])
+        subset.append(subset[idx])
     return subset
-
-
-
-
-
-
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,14 +1,50 @@
+"""Unit Testing"""
 __author__ = "730529946"
 
-from exercises.ex05.utils import only_evens
-from exercises.ex05.utils import concat
-from exercises.ex05.utils import sub
+from exercises.ex05.utils import only_evens, concat, sub
 
-def test_only_evens() -> None:
-    """Testing only evens."""
+def test_empty() -> None:
+    "Empty list with only evens."
+    assert only_evens([]) == []
 
-def test_concat() -> None:
-    """Testing concat."""
+def test_concat_empty() -> None:
+    """Empty list."""
+    assert concat([], []) == []
 
-def test_sub() -> None:
-    """Testing subset."""
+def even_list() -> None:
+    """Returning evens from a list."""
+    list1: list[int] = [1, 2, 3, 4]
+    assert only_evens(list1) == []
+
+def sub_empty() -> None:
+    """Subset empty."""
+    assert sub([], 2, 3) == []
+
+def negative_start() -> None:
+    """Starting with a negative."""
+    list2: list[int] = [1, 2, 3, 4]
+    starting: int = -1
+    ending: 4
+    assert sub(list2, starting, ending) == [1, 2, 3, 4]
+
+def negative() -> None:
+    """Negative"""
+    list3: list[int] = [-11, -4, -3, 2, 1]
+    assert only_evens(list3) == [-11, 10]
+
+def two_lists() -> None:
+    """Two list test."""
+    list4: list[int] = ([1, 2, 3], [1, 2, 3])
+    assert concat(list4) == []
+
+def single_list() -> None:
+    """A single list and an empty."""
+    list5: list[int] = ([1, 2, 3], [])
+    assert concat(list5) == []
+
+def end_negative() -> None:
+    """Ending with negative"""
+    list6: list[int] = [4, 3, 2, 1]
+    starts: int = 4
+    ends: int = -1
+    assert sub(list6, starts, ends) == [4, 3, 2, 1]
