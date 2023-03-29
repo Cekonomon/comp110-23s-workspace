d25a18566c410bd8d4c34113730752b03284928a
camryn ekonomon <cekonomon@unc.edu>
2023-03-29 19:31:36 -0400

    Progress on Exercise 3

---
+++ exercises/ex07/dictionary.py
@@ -0,0 +1,40 @@
+"""Ex07 dictionary"""
+
+__author__ = "730529946"
+
+def invert(input_list: dict[str, str]) -> dict[str, str]:
+    """Invert the key values of a dictionary."""
+    inverted = {}
+    for word in input_list:
+        if input_list[word] in inverted:
+            raise KeyError("Error found")
+        inverted[input_list[word]] = word
+    return inverted
+
+def favorite_color(colors: dict[str, str]) -> str:
+    """Returns the favorite color."""
+    appears = {}
+    for word in colors:
+        color = colors[word]
+        if color in appears:
+            appears[color] += 1
+        else:
+            appears[color] = 1
+
+    count = 0
+    popular = {}
+    for color in appears:
+        if appears[color] > count:
+            count = appears[color]
+            popular = color
+    return popular
+
+def count(items: list[str]) -> dict[str, int]:
+    """Counts the frequencies."""
+    input_list = {}
+    for item in items:
+        if item in input_list:
+            input_list[item] += 1
+        else:
+            input_list[item] = 1
+    return input_list
---
+++ exercises/ex07/dictionary_test.py
@@ -0,0 +1,52 @@
+"""Pytest for dictionary."""
+__author__ = "730529946"
+
+from exercises.ex07.dictionary import invert, favorite_color, count
+
+
+def test_invert_one() -> None:
+    """Testing with single letters."""
+    input_list = {'a': 'z', 'b': 'y', 'c': 'x'}
+    assert invert(input_list) == {'z': 'a', 'y': 'b', 'x': 'c'}
+
+def test_invert_two() -> None:
+    """Testing with words."""
+    input_list = {'apple': 'cat'}
+    assert invert(input_list) == {'cat': 'apple'}
+
+import pytest
+def test_invert_three() -> None:
+    """Raise keyerror."""
+    with pytest.raises(KeyError):
+        input_list = {'kris': 'jordan', 'michael': 'jordan'}
+        invert(input_list)
+
+def test_favorite_color_one():
+    """Testing two colors."""
+    assert favorite_color({"Marc": "yellow", "Ezri": "blue", "Kris": "blue"}) == 'blue'
+
+def test_favorite_color_two() -> None:
+    """Testing with duplicate favorties"""
+    assert favorite_color({"three": "yellow", "two": "blue", "one": "blue", "four": "yellow"}) == 'yellow'
+
+def test_favorite_color_three() -> None:
+    """Testing another case of one favorite color."""
+    assert favorite_color({"Cam": "red", "Alex": "blue", "Alyssa": "blue"}) == 'blue'
+
+def test_count_one() -> None:
+    """Testing an empty case."""
+    items = []
+    result = {}
+    assert count(items) == result
+
+def test_count_three() -> None:
+    """Counting repeated items in the list"""
+    items = ['soccer', 'soccer', 'lacrosse', 'lacrosse']
+    result = {'soccer': 2, 'lacrosse': 2}
+    assert count(items) == result
+
+def test_count_two() -> None:
+    """Testing only one"""
+    items = ['soccer', 'soccer', 'soccer', 'soccer']
+    result = {'soccer': 4}
+    assert count(items) == result 
